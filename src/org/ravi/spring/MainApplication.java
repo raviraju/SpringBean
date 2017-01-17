a4fdb9255727e37bced7ae1aecbe8cf4b8fb692a
Ravi <Ravi@10.0.0.178>
2017-01-17 09:23:47 -0800

    check2

--- src/org/ravi/spring/MainApplication.java
+++ src/org/ravi/spring/MainApplication.java
@@ -17,7 +17,6 @@ public class MainApplication {
 		//Triangle t = (Triangle) factory.getBean("triangle");
 		//t.draw();
 		
-		
 		/*The ApplicationContext container includes all functionality of the BeanFactory container, 
 		so it is generally recommended over the BeanFactory. 
 		BeanFactory can still be used for light weight applications like mobile devices or applet based applications where data volume and speed is significant.
