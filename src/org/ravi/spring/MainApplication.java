d44ede71474a306f64d9d4bc0c215d6858e908dc
Ravi <Ravi@10.0.0.178>
2017-01-17 09:16:45 -0800

    Spring Bean Sample Application
    https://www.tutorialspoint.com/spring/spring_hello_world_example.htm
    
    https://www.youtube.com/watch?v=7c6ZTF6cF88

---
+++ src/org/ravi/spring/MainApplication.java
@@ -0,0 +1,29 @@
+package org.ravi.spring;
+
+//import org.springframework.beans.factory.BeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.core.io.FileSystemResource;
+
+//@SuppressWarnings("deprecation")
+public class MainApplication {
+
+	public static void main(String[] args) {
+		//Triangle t = new Triangle();
+		//t.draw();
+		
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		//Triangle t = (Triangle) factory.getBean("triangle");
+		//t.draw();
+		
+		/*The ApplicationContext container includes all functionality of the BeanFactory container, 
+		so it is generally recommended over the BeanFactory. 
+		BeanFactory can still be used for light weight applications like mobile devices or applet based applications where data volume and speed is significant.
+		*/
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle t = (Triangle) context.getBean("triangle");
+		t.draw();
+	}
+
+}
---
+++ src/org/ravi/spring/Triangle.java
@@ -0,0 +1,14 @@
+package org.ravi.spring;
+
+public class Triangle {
+	private String type;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void draw(){
+		System.out.println(type + " Triangle drawn");
+	}
+}
