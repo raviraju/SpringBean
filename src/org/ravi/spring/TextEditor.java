4e54363afc895dd94e5c9265b8d635b65fb96f94
Ravi <Ravi@10.0.0.178>
2017-01-17 13:54:32 -0800

    Spring Setter-based Dependency Injection
    https://www.tutorialspoint.com/spring/setter_based_dependency_injection.htm

--- src/org/ravi/spring/TextEditor.java
+++ src/org/ravi/spring/TextEditor.java
@@ -3,9 +3,19 @@ package org.ravi.spring;
 public class TextEditor {
 	private SpellChecker spellChecker;
 
-   public TextEditor(SpellChecker spellChecker) {
+   /*public TextEditor(SpellChecker spellChecker) {
       System.out.println("Inside TextEditor constructor." );
       this.spellChecker = spellChecker;
+   }*/
+	
+   // a setter method to inject the dependency.
+   public void setSpellChecker(SpellChecker spellChecker) {
+      System.out.println("Inside setSpellChecker." );
+      this.spellChecker = spellChecker;
+   }
+   // a getter method to return spellChecker
+   public SpellChecker getSpellChecker() {
+      return spellChecker;
    }
    public void spellCheck() {
       spellChecker.checkSpelling();
